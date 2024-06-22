a32ad16aab145a7f9dbc3e483d8cc0014b39047c
MiqueiasSantiago <Miqueiasengsoft@gmail.com>
2024-06-22 16:37:27 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/cursojava/course/entities/User.java
+++ src/main/java/com/cursojava/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.cursojava.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,15 +20,18 @@ public class User implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	public User() {
 	}
-	public User(Integer id, String name, String email, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -34,10 +40,10 @@ public class User implements Serializable {
 		this.password = password;
 	}
 	
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 	public String getName() {
@@ -63,8 +69,11 @@ public class User implements Serializable {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}	
+	public List<Order> getOrders() {
+		return orders;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
