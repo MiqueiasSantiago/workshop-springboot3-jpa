82a83890c14be159f5fd7118f15af6fb88caff9d
MiqueiasSantiago <Miqueiasengsoft@gmail.com>
2024-06-25 15:31:54 -0300

    Subtotal & Total methods

--- src/main/java/com/cursojava/course/entities/OrderItem.java
+++ src/main/java/com/cursojava/course/entities/OrderItem.java
@@ -58,6 +58,10 @@ public class OrderItem implements Serializable{
 		id.setProduct(product);;
 	}
 	
+	public Double getSubTotal() {
+		return price * quantity;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
