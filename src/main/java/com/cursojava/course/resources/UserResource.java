b653c43cc78b42b2cba1eb827e01b5701e90201d
MiqueiasSantiago <Miqueiasengsoft@gmail.com>
2024-06-25 16:12:35 -0300

    User delete

--- src/main/java/com/cursojava/course/resources/UserResource.java
+++ src/main/java/com/cursojava/course/resources/UserResource.java
@@ -3,10 +3,9 @@ package com.cursojava.course.resources;
 import java.net.URI;
 import java.util.List;
 
-import javax.tools.DocumentationTool.Location;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +16,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cursojava.course.entities.User;
 import com.cursojava.course.services.UserService;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -44,5 +42,11 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
